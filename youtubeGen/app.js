554a6289354411b4ee9d43faa8a9e4986751abf3
Koding <trenton.daniels@gmail.com>
2015-09-23 20:01:04 +0000

    added youtube api

---
+++ youtubeGen/app.js
@@ -0,0 +1,36 @@
+$('#submit-search').click(function() {
+    var searchItem = $('#search-item').val();
+    var everything = "<ul>";
+    console.log(everything);
+    var myurl= "https://content.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCUdNPjVYJln_JIlm31tbAmdK24-XYNGsQ&q=";
+    myurl += searchItem;
+    //myurl += ".json";
+    console.log(myurl);
+        $.ajax({
+            type: "GET",
+            url: myurl,
+            cache: false,
+            dataType:'jsonp',
+            success: function(parsed_json){
+                console.log(parsed_json);
+                var title = parsed_json.items[1].snippet.title;
+                var channel = parsed_json.items[1].snippet.channelTitle;
+                var description = parsed_json.items[1].snippet.description;
+                var id = parsed_json.items[1].id.videoId;
+                var videoSource= "https://www.youtube.com/watch?v=" + id;
+                console.log(everything);
+                everything += "<li>Title: "+title +"</li>";
+                everything += "<li>ID: "+id +"</li>";
+                everything += "<li>Channel: "+channel +"</li>";
+                everything += "<li>Description: "+description +"</li>";
+                everything += "</ul>";
+                console.log(everything);
+                $("#results").html(everything);
+                $('#yt-player iframe').remove();
+                $('<iframe height="500px" width="1000px" frameborder="0" allowfullscreen></iframe>')
+                .attr("src", "http://www.youtube.com/embed/" + id)
+                .appendTo("#yt-player");
+            }
+        });
+    return false;
+});
