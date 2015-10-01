6c63d9c278db338e0b64d91b29d0de0f415eafdb
Koding <trenton.daniels@gmail.com>
2015-10-01 15:20:00 +0000

    Add new folders test

---
+++ familyHistory/js/controllers/MainController.js
@@ -0,0 +1,32 @@
+app.controller('familyCtrl', function($scope) {
+    $scope.personItems = 
+    [
+        {
+            person: "Trenton Daniels",
+            father: "Christopher Daniels",
+            mother: "Cleone Pearce",
+            done: false
+        }
+    ]
+    
+    
+    $scope.familyAdd = function() {
+        $scope.personItems.push(
+            {
+                person: $scope.personInput,
+                father: $scope.fatherInput,
+                mother: $scope.motherInput,
+                done: false
+                
+            });
+        $scope.familyInput = "";
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.personItems;
+        $scope.personItems = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.personItems.push(x);
+        });
+    };
+});
